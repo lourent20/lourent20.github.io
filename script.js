743c1ec35db307b2ae5985be6312607b0a7dff8f
lourent20 <85454150+lourent20@users.noreply.github.com>
2021-09-21 22:18:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Student", "Werktuigbouwkundige", "Techneut", "Werktuigbouwkundige"],
+        strings: ["Student", "Werktuigbouwkundige", "Techneut"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Student", "Werktuigbouwkundige", "Techneut", "Werktuigbouwkundige"],
+        strings: ["Student", "Werktuigbouwkundige", "Techneut"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
